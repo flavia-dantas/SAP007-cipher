1b683dd9f77d1e007f7e7efdf332fa1ffe803e3c
flavia-dantas <flavia.dantas2@hotmail.com>
2022-01-23 17:58:06 -0300

    função botão criptografar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import cipher from './cipher.js';
 
+
+
+const bntEncode = document.getElementById("btnEncode").addEventListener("click",cripitografar)
+
+function cripitografar(evento){
+    evento.preventDefault();
+    let txtEncode = document.getElementById("msgCodificada").value;
+    let offsetEncode = document.getElementById("offsetEncode").value;
+    
+    console.log(txtEncode);
+    console.log(offsetEncode);
+}
+
+
 console.log(cipher);

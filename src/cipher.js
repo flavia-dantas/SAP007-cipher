a7365b7f9cc7cc7712f902cfdbc48143976c16c7
flavia-dantas <flavia.dantas2@hotmail.com>
2022-01-27 10:47:28 -0300

    Função Cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -2,27 +2,26 @@
 // direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
 // cipher.decode(offset, string): offset é o número de posições que queremos mover para a 
 // esquerda no alfabeto e string é a mensagem (texto) que queremos decifrar.
+
 //(codigoDaletraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
 
-// const tamDoAlfabeto = 26
-// const codigoASC = 65
+const tamDoAlfabeto = 26
+const codigoASC = 65
 
 const cipher = {
 
-//   encode: function (offset, msg){
+  encode (offset, mensagem){
+    let resultadoCodificar = "";
+    for (let i = 0; i < mensagem.length ; i++){
+    let valorASC = mensagem.charCodeAt(i);
+    let codificando = ((valorASC - codigoASC + offset) % tamDoAlfabeto) + codigoASC;
+    resultadoCodificar += String.fromCharCode(codificando);
     
-    
-    //((codigoASC - msg + offset) % tamDoAlfabeto) + msg;
-    // let resultadoCodificar = ""
-    // for (let i = 0; msg.length; i++){
-    //   resultadoCodificar.charCodeAt(i)
-      
-    
+   }
+    return resultadoCodificar;
+    //console.log(resultadoCodificar);
+  },
   
-  //}
 
 };
-
 export default cipher;
-
-
--- src/index.js
+++ src/index.js
@@ -4,15 +4,30 @@ import cipher from './cipher.js';
 
 const btnEncode = document.getElementById("btnEncode").addEventListener("click",cripitografar)
 const btnDecode = document.getElementById("btnDecode").addEventListener("click",descriptografar)
+const textoEncode = document.getElementById("msgCodificada").addEventListener("change",maiusculaEncode) // change = mudança
+// const btnLimparEncode = document.getElementById("btnLimparEncode").addEventListener("click",limparEncode)
+// const btnLimparDecode = document.getElementById("btnLimparDecode").addEventListener("click",limparDecode)
 
-function cripitografar(evento){
-    evento.preventDefault();
-    let txtEncode = document.getElementById("msgCodificada").value;
-    let offsetEncode = document.getElementById("offsetEncode").value;
+function maiusculaEncode(){
+    let converteTxtEncode = document.getElementById("msgCodificada").value;
+    document.getElementById("msgCodificada").value = converteTxtEncode.toUpperCase();
     
-    console.log(txtEncode);
-    console.log(offsetEncode);
 }
+function cripitografar(event){
+    event.preventDefault();
+    let msgCodificada = document.getElementById("msgCodificada").value;
+    // document.getElementById("msgCodificada").value = msgCodificada.toUpperCase();
+    let offsetEncode = parseInt(document.getElementById("offsetEncode").value);
+    const cripto = cipher.encode(offsetEncode, msgCodificada);
+    document.getElementById("resultadoEncode").value = cripto;
+
+    console.log(msgCodificada);
+    console.log(offsetEncode);
+    console.log(cripto);
+} 
+
+
+    
 
 function descriptografar(evento){
     evento.preventDefault();
@@ -22,4 +37,18 @@ function descriptografar(evento){
     console.log(txtDecode);
     console.log(offsetDecode);
 }
+
+// function limparEncode(){
+//     document.getElementById("msgCodificada").value ="";
+//     document.getElementById("offsetEncode").value = "";
+//     document.getElementById("resultadoEncode").value ="";
+
+// }
+
+// function limparDecode(){
+//     let txtLimparDecode = document.getElementById("msgDescodificada").value;
+
+//     console.log(txtLimparDecode);
+// }
+
 console.log(cipher);

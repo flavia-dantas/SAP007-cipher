e78af2c8fb6d49aec74f09cf1502ca83723c699c
flavia-dantas <flavia.dantas2@hotmail.com>
2022-01-30 17:30:19 -0300

    Exclusão btn limpar

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const btnEncode = document.getElementById("btnEncode")
 const btnDecode = document.getElementById("btnDecode")
 const textoEncode = document.getElementById("msgCodificada")
 const textoDecode = document.getElementById("msgDescodificada") 
-// const btnLimparEncode = document.getElementById("btnLimparEncode").addEventListener("click",limparEncode)
-// const btnLimparDecode = document.getElementById("btnLimparDecode").addEventListener("click",limparDecode)
+
 
 function maiusculaEncode(){
     let converteTxtEncode = document.getElementById("msgCodificada").value;
@@ -17,14 +16,11 @@ function maiusculaEncode(){
 
 function cripitografar(event){
     event.preventDefault();
-    let msgCodificada = document.getElementById("msgCodificada").value;
+    let msgCodificada = document.getElementById("msgCodificada").value.toUpperCase();
     let offsetEncode = parseInt(document.getElementById("offsetEncode").value);
     const cripto = cipher.encode(offsetEncode, msgCodificada);
     document.getElementById("resultadoEncode").value = cripto;
 
-    // console.log(msgCodificada);
-    // console.log(offsetEncode);
-    // console.log(cripto);
 } 
     btnEncode.addEventListener("click",cripitografar)
 
@@ -35,29 +31,12 @@ function maiusculaDecode(){
 }
     textoDecode.addEventListener("change",maiusculaDecode) // change = mudança
 
-function descriptografar(evento){
-    evento.preventDefault();
-    let msgDescodificada = document.getElementById("msgDescodificada").value;
+function descriptografar(event){
+    event.preventDefault();
+    let msgDescodificada = document.getElementById("msgDescodificada").value.toUpperCase();
     let offsetDecode = document.getElementById("offsetDecode").value;
     const descripto = cipher.decode(offsetDecode,msgDescodificada);
     document.getElementById("resultadoDecode").value = descripto;
     
-    // console.log(msgDescodificada);
-    // console.log(offsetDecode);
-    // console.log(descripto);
 }
-    btnDecode.addEventListener("click",descriptografar)
-// function limparEncode(){
-//     document.getElementById("msgCodificada").value ="";
-//     document.getElementById("offsetEncode").value = "";
-//     document.getElementById("resultadoEncode").value ="";
-
-// }
-
-// function limparDecode(){
-//     let txtLimparDecode = document.getElementById("msgDescodificada").value;
-
-//     console.log(txtLimparDecode);
-// }
-
-//console.log(cipher);
+    btnDecode.addEventListener("click",descriptografar);

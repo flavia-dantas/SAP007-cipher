0a4edda6d2465dbb964560c58c276923f5b5396d
flavia-dantas <flavia.dantas2@hotmail.com>
2022-02-01 11:42:13 -0300

    Add TypeError

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,12 @@
-// cipher.encode(offset, string): offset é o número de posições que queremos mover para a 
-// direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
-// cipher.decode(offset, string): offset é o número de posições que queremos mover para a 
-// esquerda no alfabeto e string é a mensagem (texto) que queremos decifrar.
-
-//(codigoDaletraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
-
 const tamDoAlfabeto = 26
 const codigoASC = 65
-
 
 const cipher = {
 
   encode: function (offset, mensagem){
-   
+    if (offset == null || offset == "" || mensagem == null || mensagem ==""){ //throw lança uma exceção definida pelo usuário
+      throw new TypeError("Verificar o offset ou a mensagem"); // TypeError é lançado quando um parâmetro de uma função não é do tipo desejado
+    }
     let resultadoCodificar = "";
     for (let i = 0; i < mensagem.length; i++){
     let letrasCifrada = mensagem.charCodeAt(i);
@@ -25,6 +19,9 @@ const cipher = {
   },
   
   decode: function (offset, mensagem){
+    if (offset == null || offset == "" || mensagem == null || mensagem ==""){ //throw lança uma exceção definida pelo usuário
+      throw new TypeError("Verificar o offset ou a mensagem"); // TypeError é lançado quando um parâmetro de uma função não é do tipo desejado
+    }
     let resultadoDescodificar ="";
     for (let i = 0; i < mensagem.length; i++){
     let letrasDecifrada = mensagem.charCodeAt(i);
@@ -36,3 +33,8 @@ const cipher = {
 
 };
 export default cipher;
+
+// cipher.encode(offset, string): offset é o número de posições que queremos mover para a 
+// direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
+// cipher.decode(offset, string): offset é o número de posições que queremos mover para a 
+// esquerda no alfabeto e string é a mensagem (texto) que queremos decifrar.
